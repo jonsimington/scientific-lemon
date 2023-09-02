18f4b0fd958445d49765dbac4928b02f39ecf717
Jon Simington <jon@simington.io>
2023-09-02 00:13:42 -0500

    Update player name

--- games/chess/ai.py
+++ games/chess/ai.py
@@ -28,7 +28,7 @@ class AI(BaseAI):
 			str: The name of your Player.
 		"""
 
-        return "Tim Buesking"  # REPLACE THIS WITH YOUR TEAM NAME
+        return "Scientific Lemon"  # REPLACE THIS WITH YOUR TEAM NAME
 
     def start(self):
         """ This is called once the game starts and your AI knows its playerID
